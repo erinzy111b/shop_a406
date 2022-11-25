ccbd6d653bec22a9fa3550a3e0e29e3be976a4d6
javckzack <javck.zack@gmail.com>
2022-11-25 16:17:38 +0800

    共用變數示範

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -178,7 +178,7 @@
                             <!-- social -->
                             <div class="footer-social">
                                 <a href="#"><i class="fab fa-twitter"></i></a>
-                                <a href="https://www.facebook.com/sai4ull"><i class="fab fa-facebook-f"></i></a>
+                                <a href="https://www.facebook.com/{{$fb_id}}"><i class="fab fa-facebook-f"></i></a>
                                 <a href="#"><i class="fab fa-behance"></i></a>
                                 <a href="#"><i class="fas fa-globe"></i></a>
                             </div>
